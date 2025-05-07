e30b1874a4f9ae1fb9e541cba911f045f5b30bb8
Lindsay Marín Sánchez <168288561+CholiRat@users.noreply.github.com>
2025-05-06 18:29:56 -0600

    Update View.sql

--- View.sql
+++ View.sql
@@ -35,7 +35,7 @@ BEGIN
 	INNER JOIN dbo.Solt_Users u ON i.userid = u.userid
     WHERE redemptionStatusid = 2;
 
-	SET @computer = 'PC de ' + @username
+	SET @computer = 'PC de ' + @username;
 	SET @concatString = CAST(@description AS NVARCHAR) + 
                         CAST(@computer AS NVARCHAR) + 
                         CAST(@username AS NVARCHAR) + 
